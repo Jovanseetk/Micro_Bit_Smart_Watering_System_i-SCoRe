f4fb0016b0d0d689e911f72118e2dd4cb7e5d14f
Jovanseetk <73521844+Jovanseetk@users.noreply.github.com>
2020-11-05 22:00:24 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+radio.setTransmitSerialNumber(true)
+radio.setGroup(1)
+led.setBrightness(64)
+basic.forever(function () {
+    if (true) {
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        if (true) {
+        	
+        }
+    }
+})
